bd0cd8c5618fa42968568244e954b97b66f5fe9b
TamWilliam <72071669+TamWilliam@users.noreply.github.com>
2023-01-08 22:49:06 +0100

    delete validators.js

--- src/validators.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup"
-
-export const todonameValidator = yup
-  .string()
-  .matches(
-    /^[a-z][a-z0-9_]{1,}$/,
-    "Your todoname must start with a lower case letter and contain only digits or letters or `_`"
-  )
-  .label("Todoname")
